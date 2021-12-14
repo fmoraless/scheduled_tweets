0982dec523fb6caaff53704e074127f46c8ef455
Francisco <fcomorales.sanchez@gmail.com>
2021-12-14 19:03:00 -0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   # GET /about
   get "about", to: "about#index"
 
+  get "sign_up", to: "registrations#new"
+  post "users", to: "registrations#create"
+
   root to: "main#index"
 
 end
