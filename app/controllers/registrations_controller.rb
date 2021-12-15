0378ea63325b28e7fa0d54bfb7e5d9c223cb7aa2
Francisco <fcomorales.sanchez@gmail.com>
2021-12-15 09:06:36 -0300

    send user parameters

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,5 +5,14 @@ class RegistrationsController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, notice: "Successful created account"
+    else
+      render :new
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
